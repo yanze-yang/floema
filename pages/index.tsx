7b825ca9585be9a33738b45c51eea88a730dc603
Ryan Yang <ryanyang.nz@gmail.com>
2022-11-29 12:24:44 +1300

    Fix <a> tag to <Link />

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -22,10 +23,10 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/collection" className={styles.card}>
+          <Link href="/collection" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
