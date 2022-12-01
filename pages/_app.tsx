5ead13d74294a6febaae6eccdab48bf56921e8b4
Ryan Yang <ryanyang.nz@gmail.com>
2022-12-01 19:14:47 +1300

    add global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,21 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "George X";
+    src: url("/fonts/George X.otf") format("otf");
+    font-style: normal;
+    font-display: swap;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   );
